0c284894d953c6ec016c7effcc80db032af40881
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-06-18 18:48:36 +0300

    paskaita 8

--- React/Paskaita8/backend/server.js
+++ React/Paskaita8/backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "./router.js";
 
 dotenv.config();
 
@@ -11,3 +12,11 @@ mongoose
   .connect(MONGO_URI, { dbName: "todo" })
   .then(() => console.log("Connected to DB"))
   .catch(() => console.log("Failed to connect to DB"));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router)
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
