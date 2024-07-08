cfeade9cf5de86cbc8ce01624b9eb73215a46afe
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-07-08 19:59:03 +0300

    use query

--- React/Paskaita19/useQuery/src/components/PostForm/PostForm.jsx
+++ React/Paskaita19/useQuery/src/components/PostForm/PostForm.jsx
@@ -1,28 +1,45 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 const API_HOST = "https://jsonplaceholder.typicode.com/posts";
 
 export default function PostForm() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   const queryClient = useQueryClient();
 
-
   const { mutate, isError, error } = useMutation({
-    mutationFn: async () => {
-      await axios.post(API_HOST, { title: "asdasdf", body: "alkasjndfkljansdfk" });
+    mutationFn: async (body) => {
+      await axios.post(API_HOST, body);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["posts"] })
-    }
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
   });
 
-  if(isError) {
-    return <h1>Some error happened while posting</h1>
+  if (isError) {
+    return <h1>Some error happened while posting</h1>;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const requestBody = {
+      title,
+      body
+    }
+
+    mutate(requestBody)
   }
 
   return (
     <div>
-      <button onClick={mutate}>Create new post</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} /> <br />
+        <input type="text" value={body} onChange={(e) => setBody(e.target.value)} />
+        <button type="submit">Create new post</button>
+      </form>
     </div>
   );
 }
